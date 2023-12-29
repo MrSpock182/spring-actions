816a5f6a07dca6fabb30ae2e63bfb336c6637ca2
MrSpock182 <klebernunes182@gmail.com>
2023-12-29 18:40:31 -0300

    alteracao da mensagem

--- src/main/java/io/github/mrspock182/actions/resource/HelloController.java
+++ src/main/java/io/github/mrspock182/actions/resource/HelloController.java
@@ -14,6 +14,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String getHello() {
-        return "Olá " + name + ", com você esta?";
+        return "Olá " + name + ", com você esta? Gosteria de realizar um teste";
     }
 }
