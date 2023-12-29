6de716815e579f9ba3353e6ae3bc601976abefec
MrSpock182 <klebernunes182@gmail.com>
2023-12-29 00:28:05 -0300

    colocando variavel

--- src/main/java/io/github/mrspock182/actions/HelloController.java
+++ src/main/java/io/github/mrspock182/actions/HelloController.java
@@ -1,12 +1,19 @@
 package io.github.mrspock182.actions;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private final String name;
+
+    public HelloController(@Value("${actions.name}") String name) {
+        this.name = name;
+    }
+
     @GetMapping("/hello")
     public String getHello() {
-        return "Hello Yurika, com você esta?";
+        return "Hello " + name + ", com você esta?";
     }
 }
