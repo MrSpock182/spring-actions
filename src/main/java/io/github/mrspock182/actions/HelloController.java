f9754c2fd3d06e2f065a4472603c22660cfb86ec
MrSpock182 <klebernunes182@gmail.com>
2023-12-28 21:24:09 -0300

    first commit

---
+++ src/main/java/io/github/mrspock182/actions/HelloController.java
@@ -0,0 +1,12 @@
+package io.github.mrspock182.actions;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String getHello() {
+        return "Hello Yurika";
+    }
+}
---
+++ src/test/java/io/github/mrspock182/actions/ActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mrspock182.actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
